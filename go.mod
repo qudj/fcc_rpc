c7b8e210e677afa431086c1c3798a5e1fa2abdd2
qudongjie <qudongjie@bituniverse.org>
2021-12-08 22:33:34 +0800

    change

--- go.mod
+++ go.mod
@@ -4,10 +4,9 @@ go 1.17
 
 require (
 	github.com/gogo/protobuf v1.3.2
-	github.com/grpc-ecosystem/go-grpc-middleware v1.3.1-0.20211019021415-560829fc74fc
 	github.com/qudj/fly_lib v0.0.0-20211208061117-97556541bd37
+	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.9.0
-	go.uber.org/zap v1.19.1
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	google.golang.org/grpc v1.42.0
 	gorm.io/driver/mysql v1.2.1
@@ -19,6 +18,7 @@ require (
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.1-0.20211019021415-560829fc74fc // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.3 // indirect
@@ -32,6 +32,7 @@ require (
 	github.com/subosito/gotenv v1.2.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
 	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
 	golang.org/x/text v0.3.6 // indirect
--- middlewares/access.log.go
+++ middlewares/access.log.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"context"
 	"encoding/json"
+	uuid "github.com/satori/go.uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
 	"log"
@@ -10,13 +11,16 @@ import (
 	"time"
 )
 
-
 func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	remote, _ := peer.FromContext(ctx)
 	remoteAddr := remote.Addr.String()
 	pos := strings.LastIndex(info.FullMethod, "/") + 1
 	method := info.FullMethod[pos:]
-
+	traceId, ok := ctx.Value("trace_id").(string)
+	if !ok {
+		traceId = uuid.NewV4().String()
+		ctx = context.WithValue(ctx, "trace_id", traceId)
+	}
 	in, _ := json.Marshal(req)
 	inStr := string(in)
 
@@ -25,7 +29,11 @@ func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.Una
 		out, _ := json.Marshal(resp)
 		outStr := string(out)
 		duration := int64(time.Since(start) / time.Millisecond)
-		log.Println(" ip:", remoteAddr, " method:", method, " in:", inStr, " out:", outStr, " err:", err, " duration/ms:", duration)
+
+		log.Println(" trace_id:", traceId, " ip:", remoteAddr, " method:", method, " in:", inStr, " out:", outStr, " err:", err, " duration/ms:", duration)
+		if err != nil {
+			log.Fatal()
+		}
 	}()
 	resp, err = handler(ctx, req)
 	return
